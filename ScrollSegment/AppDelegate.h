a16cb2134a28ec40c0345404ab5f7043453d4777
Superman <703353105@qq.com>
2018-07-23 14:28:11 +0800

    Initial Commit

---
+++ ScrollSegment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollSegment
+//
+//  Created by Superman on 2018/7/23.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollSegment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollSegment
+//
+//  Created by Superman on 2018/7/23.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
